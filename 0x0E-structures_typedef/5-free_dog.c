e06947dd3aa0e7b4ee13193e4a5405026906da02
Reignel <alajembareignel@gmail.com>
2022-10-10 09:08:09 -0700

    My dog

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 
 /*
- * free_dog - Free dog memory
- * @d: Dog
+ * free_dog - frees a memory block of type dog_t
+ * @d: address to the memory block to be freed
+ * Return: void
  */
 
 void free_dog(dog_t *d)
