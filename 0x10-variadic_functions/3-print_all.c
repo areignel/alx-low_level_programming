99c1fc8a8e1bd26af56b3d6e62e6ce4b5ed525d4
Reignel <alajembareignel@gmail.com>
2022-10-15 14:23:33 -0700

    Variadic functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,5 +1,12 @@
 #include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
 
+void print_char(va_list arg);
+void print_int(va_list arg);
+void print_float(va_list arg);
+void print_string(va_list arg);
+void print_all(const char * const format, ...);
 
 /**
  * print_char - Prints a char.
@@ -11,7 +18,6 @@ void print_char(va_list arg)
 	char letter;
 
 	letter = va_arg(arg, int);
-
 	printf("%c", letter);
 }
 
@@ -25,10 +31,8 @@ void print_int(va_list arg)
 	int num;
 
 	num = va_arg(arg, int);
-
 	printf("%d", num);
 }
-
 
 /**
  * print_float - Prints a float.
@@ -40,11 +44,8 @@ void print_float(va_list arg)
 	float num;
 
 	num = va_arg(arg, double);
-
 	printf("%f", num);
 }
-
-
 
 /**
  * print_string - Prints a string.
@@ -56,29 +57,27 @@ void print_string(va_list arg)
 	char *str;
 
 	str = va_arg(arg, char *);
-
 	if (str == NULL)
 	{
 		printf("(nil)");
 		return;
 	}
-
 	printf("%s", str);
 }
 
 /**
- *print_all - prints anything
- *@format: format of input
- *Return: nothing
+ * print_all - Prints anything, followed by a new line.
+ * @format: A string of characters representing the argument types.
+ * @...: A variable number of arguments to be printed.
+ * Description: Any argument not of type char, int, float,
+ * or char * is ignored.
+ * If a string argument is NULL, (nil) is printed instead.
  */
 void print_all(const char * const format, ...)
 {
 	va_list args;
-
 	int i = 0, j = 0;
-
 	char *separator = "";
-
 	printer_t funcs[] = {
 		{"c", print_char},
 		{"i", print_int},
@@ -87,7 +86,6 @@ void print_all(const char * const format, ...)
 	};
 
 	va_start(args, format);
-
 	while (format && (*(format + i)))
 	{
 		j = 0;
@@ -103,10 +101,9 @@ void print_all(const char * const format, ...)
 		}
 
 		i++;
-
 	}
 
-	printf("\n");
 
+	printf("\n");
 	va_end(args);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,26 +1,30 @@
-#ifndef VARIADIC_FUNCTIONS_H_
-#define VARIADIC_FUNCTIONS_H_
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+/*
+ * File: variadic_functions.h
+ * Desc: Header file containing prototypes for all functions
+ * used in the 0x0F-variadic_functions directory.
+ */
+
 #include <stdarg.h>
 
-int _putchar(char c);
+/**
+ * struct printer - A new struct type defining a printer.
+ * @symbol: A symbol representing a data type.
+ * @print: A function pointer to a function that prints
+ * a data type corresponding to symbol.
+ */
+typedef struct printer
+{
+	char *symbol;
+	void (*print)(va_list arg);
+
+} printer_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
-
-/**
- * struct format_struct - format-type class
- * @format: format identifier
- * @fmt_print_func: pointer to function that prints in the format of @format
- */
-
-struct format_struct
-{
-	char format;
-	void (*fmt_print_func)(va_list *arg_list);
-};
-
-typedef struct format_struct format_struct_ptr;
 
 #endif
